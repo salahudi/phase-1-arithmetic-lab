c63dd08544b87d265ae704188f5dccab2919e555
salahudin <salahudin.salat@student.moringaschool.com>
2022-08-16 22:30:25 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply)
+ let a = 0;
+ let random = ++a;
+ console.log(random)
+ let num4 = 6
+ let num3 = 16
+ let mod = (num3 % num4);
+ console.log(mod)
+
+ let max = (10, 12, 14, 16, 18, 20);
+ Math.max;(max)
